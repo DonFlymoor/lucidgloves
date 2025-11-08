5fad63e1ffd78cf5b6d352c4025d3413daf490ec
Don Flymoor <36929174+DonFlymoor@users.noreply.github.com>
2025-11-08 18:45:44 -0500

    Refactor WIFI configuration constants in Config.h
    It was overwriting every every mention of port, password, host, and ssid in the sdk. This changes it to use the same naming convention that doesn't break everything

--- firmware/lucidgloves-firmware/Config.h
+++ firmware/lucidgloves-firmware/Config.h
@@ -15,10 +15,10 @@
 
 // serial over WIFI
 #if COMMUNICATION == COMM_WIFISERIAL
-#define ssid "Your_Wifi_SSID";
-#define password "Your_Wifi_Password";
-#define host "Your_host_ip";
-#define port 65432; // port of the tcp server
+#define WIFI_SSID "Your_Wifi_SSID";
+#define WIFI_PASSWORD "Your_Wifi_Password";
+#define SERVER_HOST "Your_host_ip";
+#define SERVER_PORT 65432; // port of the tcp server
 #endif
 
 //ANALOG INPUT CONFIG
